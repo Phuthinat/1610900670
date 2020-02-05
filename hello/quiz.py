63ed1267a39bb212fada8c00deadad27f5e081f4
Mek <60682928+Phuthinat@users.noreply.github.com>
2020-02-05 14:50:24 +0700

    test chang

--- hello/quiz.py
+++ hello/quiz.py
@@ -2,3 +2,12 @@ msg = "Hello world"
 msg2= "MEK"
 msg3="PHUTHINAT NUMSAKUN"
 print(msg+msg2+msg3)
+msg = "Hello world"
+msg2= "MEK"
+msg3="PHUTHINAT NUMSAKUN"
+print(msg+msg2+msg3)
+msg = "Hello world"
+msg2= "MEK"
+msg3="PHUTHINAT NUMSAKUN"
+print(msg+msg2+msg3)
+
